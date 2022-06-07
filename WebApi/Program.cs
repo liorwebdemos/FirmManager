ff8e49d723135217f2325deecd532ee877f254c3
liorwebdemos <liorwebdemos@gmail.com>
2022-06-07 23:06:40 +0300

    rename

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// builder.Services.AddScoped<IMessageWriter, MessageWriter>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
