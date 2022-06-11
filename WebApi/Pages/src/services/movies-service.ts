95da6bc41ad42dd46aed59bdb8d105b67171c2d5
liorwebdemos <liorwebdemos@gmail.com>
2022-06-11 16:03:50 +0300

    fixed (=unchanging) port on server

--- WebApi/Pages/src/services/movies-service.ts
+++ WebApi/Pages/src/services/movies-service.ts
@@ -5,19 +5,19 @@ import environment from "../../config/environment.json"; // TODO: prod/not?
 @autoinject()
 export class MoviesService
 {
-	constructor(private httpClient: HttpClient)
-	{
-		httpClient.configure(config =>
-		{
-			config
-				.useStandardConfiguration()
-				.withBaseUrl(environment.apiBaseUrl); // TODO
-		});
-	}
+  constructor(private httpClient: HttpClient)
+  {
+    httpClient.configure(config =>
+    {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl(environment.apiBaseUrl); // TODO
+    });
+  }
 
-	public getPopularMovies(): Promise<void>
-	{
-		return this.httpClient.get("movies/popular")
-			.then(response => response.json());
-	}
+  public getPopularMovies(): Promise<void>
+  {
+    return this.httpClient.get("/movies/popular")
+      .then(response => response.json());
+  }
 }
