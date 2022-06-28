b73e9b80f77cfa49a3287c51db87afc8ccfd85fc
liorwebdemos <liorwebdemos@gmail.com>
2022-06-28 20:27:31 +0300

    more comments deletion

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,8 +3,6 @@ using Newtonsoft.Json;
 using WebApi.BL.Contracts;
 using WebApi.BL.Implementation;
 using WebApi.DAL.Contracts;
-using WebApi.DAL.Helpers;
-using WebApi.DAL.Implementation;
 using WebApi.DAL.Implementation.Contexts;
 using WebApi.DAL.Implementation.Initializers;
 using WebApi.DAL.Repos.Implementation;
@@ -21,8 +19,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add DALs
-//builder.Services.AddScoped<IDALDepartments, DALDepartments>();
-//builder.Services.AddScoped<IDALWorkers, DALWorkers>();
 builder.Services.AddScoped<IGenericRepo, GenericRepo>();
 builder.Services.AddScoped<IDepartmentsRepo, DepartmentsRepo>();
 
@@ -34,8 +30,8 @@ builder.Services.AddScoped<IBLWorkers, BLWorkers>();
 builder.Services.AddDbContext<MainContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("MainConnection"))
 );
-//TODO: dev only?? https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-6.0#add-the-database-exception-filter
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-6.0#add-the-database-exception-filter
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddHttpContextAccessor();
 
@@ -64,17 +60,6 @@ if (app.Environment.IsDevelopment())
     app.UseHttpLogging();
     //app.UseBrowserLink();
 }
-
-//app.Use(async (context, next) =>
-//{
-//    context.Response.GetTypedHeaders().CacheControl =
-//        new CacheControlHeaderValue()
-//        {
-//            Public = true,
-//            MaxAge = TimeSpan.FromHours(1)
-//        };
-//    await next();
-//});
 
 app.UseHttpsRedirection();
 
